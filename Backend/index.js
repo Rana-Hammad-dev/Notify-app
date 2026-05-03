50d28f2406f38afe639576e0d47da2d0dcf16e91
Rana Hammad Ali <ranahammad.hmd@gmail.com>
2026-05-03 17:54:00 +0500

    Refactor CORS and express.json middleware usage

--- Backend/index.js
+++ Backend/index.js
@@ -2,19 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8001;
-
 const cors = require("cors");
-app.use(cors());
-app.use(express.json());
-
 app.use(cors({ origin: "*" }));
-
+app.use(express.json());
 // database connecting
 const connecting = require("./Database/Connection/connection")
-
 // routes
 const notesRoute = require("./Routes/notes.routes");
 app.use("/", notesRoute);
-
-
 app.listen(PORT, () => { console.log(`Server Started at port ${PORT}`) });
